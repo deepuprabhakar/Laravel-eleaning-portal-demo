282931bcbb651d1b0e6ffa5f02572184249fb7c3
deepu <deepupv91@gmail.com>
2016-05-10 21:44:51 +0530

    changed in migrations create subjects table

--- database/migrations/2016_04_19_053654_create_subjects_table.php
+++ database/migrations/2016_04_19_053654_create_subjects_table.php
@@ -20,7 +20,6 @@ class CreateSubjectsTable extends Migration
             $table->string('file');
             $table->unsignedInteger('course');
             $table->unsignedInteger('semester');
-            $table->string('file');
             $table->unique(array('name', 'batch', 'course', 'semester'));
             $table->foreign('course')->references('id')->on('courses')->onDelete('cascade');
             $table->timestamps();
