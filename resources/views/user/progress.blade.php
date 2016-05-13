952a095b62b3fac0fbeb35a754d15d430596dc3e
Abhirami <abhirami@aidersolutions.in>
2016-05-13 15:16:29 +0530

    user side progress

--- resources/views/user/progress.blade.php
+++ resources/views/user/progress.blade.php
@@ -31,28 +31,82 @@
             <h3 class="box-title">Progress</h3>
           </div><!-- /.box-header -->
           <div class="box-body">
-          @foreach($subject as $subjects)
-           <div class="box box-success box-solid">
-            <div class="box-header with-border">
-              <h3 class="box-title">{{ $subjects->name }}</h3>
-              <div class="box-tools pull-right">
-                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                </button>
-              </div>
-            </div>
-            <div class="box-body">
-            <div class="box-footer no-padding">
-              <ul class="nav nav-stacked">
-                <li><a href="">Discussion <span class="pull-right badge bg-aqua">
-                  Proceed to Discussion
-                </span></a></li>
-                <li><a href="">Quiz <span class="pull-right badge bg-green">Proceed to Quiz</span></a></li>
-                <li><a href="">Assignment<span class="pull-right badge bg-red">Submit Assignment </span></a></li>
-              </ul>
-            </div>
-            </div>
-            </div>
-          @endforeach
+          @if($subjects->count() == 0)
+          
+            @include('errors.empty', ['item' => $subjects, 'title' => 'data'])
+         
+          @else
+              @foreach($subjects as $subject)
+               <div class="box box-success box-solid">
+                <div class="box-header with-border">
+                  <h3 class="box-title">{{ $subject->name }}</h3>
+                  <div class="box-tools pull-right">
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                    </button>
+                  </div>
+                </div>
+                <div class="box-body">
+                <div class="box-footer no-padding">
+                  <ul class="nav nav-stacked">
+                    <li>
+                      @if(is_null($subject->discussion))
+                      <a href="{{ route('modules.show', [$semester, $subject->slug]) }}">Discussion 
+                        <span class="pull-right badge bg-aqua">
+                        Proceed to Discussion
+                        </span>
+                      </a>
+                      @else
+                      <a href="">Discussion
+                        <span class="pull-right badge bg-aqua">
+                        5
+                        </span>
+                      </a>
+                      @endif
+                    </li>
+                    <li>
+                      @if(is_null($subject->quizresult))
+                      <a href="{{ route('modules.show', [$semester, $subject->slug]) }}">Quiz 
+                        <span class="pull-right badge bg-aqua">
+                        Proceed to Quiz
+                        </span>
+                      </a>
+                      @else
+                      <a href="">Quiz
+                        <span class="pull-right badge bg-aqua">
+                        {{ $subject->quizresult->score }}
+                        </span>
+                      </a>
+                      @endif
+                    </li>
+                    <li>
+                      @if(is_null($subject->assignment))
+                      <a href="{{ route('modules.show', [$semester, $subject->slug]) }}">Assignment 
+                        <span class="pull-right badge bg-aqua">
+                        Proceed to Assignment 
+                        </span>
+                      </a>
+                      @else
+                          @if($subject->assignment->mark == 0)
+                          <a href="">Assignment 
+                            <span class="pull-right badge bg-aqua">
+                               Added but not reviewed!
+                            </span>
+                          </a>
+                          @else
+                          <a href="">Assignment 
+                            <span class="pull-right badge bg-aqua">
+                            {{ $subject->assignment->mark }}
+                            </span>
+                          </a>
+                          @endif
+                      @endif
+                    </li>
+                  </ul>
+                </div>
+                </div>
+                </div>
+              @endforeach
+          @endif
           </div>
         </div><!-- /.box -->
       </div>
